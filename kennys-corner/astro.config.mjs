bbb24ec8164a06bce9113c3dfffb9daf9498e469
Kenneth Hoff <kenneth.98@hotmail.com>
2025-02-26 20:47:50 +0100

    Set PORT correctly

--- kennys-corner/astro.config.mjs
+++ kennys-corner/astro.config.mjs
@@ -11,10 +11,12 @@ const siteUrl =
     process.env.VERCEL_ENV == "production"
         ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
         : // Otherwise, if you have a VERCEL_URL, use it
-          process.env.VERCEL_URL
-          ? `https://${process.env.VERCEL_URL}`
-          : // Otherwise, just use localhost.
+        process.env.VERCEL_URL
+            ? `https://${process.env.VERCEL_URL}`
+            : // Otherwise, just use localhost.
             "http://localhost:4321";
+
+const port = Number(process.env.PORT);
 
 export default defineConfig({
     site: siteUrl,
@@ -24,4 +26,7 @@ export default defineConfig({
     integrations: [sitemap(), react()],
     adapter: vercel(),
     trailingSlash: "never",
-});
+    ...(port && {
+        server: { port: port }
+    })
+})
