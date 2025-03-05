b30e6e9f9a4623aff85e9c503300798caf9efc89
Kenneth Hoff <kenneth.98@hotmail.com>
2025-03-05 16:11:25 +0100

    Create ServiceDefaults

--- Backend/src/KennysCorner.Api/Program.cs
+++ Backend/src/KennysCorner.Api/Program.cs
@@ -1,7 +1,14 @@
+using Marten;
+
 var builder = WebApplication.CreateSlimBuilder(args);
+builder.AddServiceDefaults();
+
 builder.Services.AddOpenApi();
+builder.Services.AddMarten();
 
 var app = builder.Build();
+app.MapDefaultEndpoints();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
